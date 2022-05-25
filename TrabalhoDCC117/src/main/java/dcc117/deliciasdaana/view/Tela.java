474de012b7ad6d90c3b2b0c11fa1a8acf1d49099
João <freitas.joaopedro@estudante.ufjf.br>
2022-05-25 14:51:39 -0300

    iniciando projeto

---
+++ TrabalhoDCC117/src/main/java/dcc117/deliciasdaana/view/Tela.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dcc117.deliciasdaana.view;
+
+/**
+ *
+ * @author joaop
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Tela extends JFrame
+{
+    
+}
